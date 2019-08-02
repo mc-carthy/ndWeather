3cfb2c9b0f44247b0692813480ac7372b142287a
Mick McCarthy <mick.mccarthy@rakuten.com>
2019-08-02 15:53:57 +0100

    Sending data to server

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => res.render('index'))
+
+app.post('/', function(req, res) {
+    res.render('index');
+    console.log(req.body.city);
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
